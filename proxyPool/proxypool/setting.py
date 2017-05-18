ab9c1801311b8ba5255e1bdd480ba62babedacc3
chonpsk <chonpskz@gmail.com>
2017-05-19 02:02:28 +0800

    version 0.0.4 zwei????

---
+++ proxyPool/proxypool/setting.py
@@ -0,0 +1,22 @@
+"""
+-------------------------------------------------
+    File Name:     setting.py
+    Description:   设置模块，包含了一些常量。
+    Author:        Liu
+    Edited by:     chonps
+-------------------------------------------------
+"""
+
+# Redis Host
+HOST = 'localhost'
+# Redis PORT
+PORT = 6379
+
+
+POOL_LOWER_THRESHOLD = 44444444
+POOL_UPPER_THRESHOLD = 44444444
+
+VAILD_CHECK_CYCLE = 600
+POOL_LEN_CHECK_CYCLE = 20
+
+TEST_URL = 'https://sanriocharacterranking.com/'
---
+++ proxyPool/proxypool/utils.py
@@ -0,0 +1,49 @@
+import requests
+import lxml
+import asyncio
+import time
+import aiohttp
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+    (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8'
+}
+
+
+def get_page(url):
+    try:
+        r = requests.get(url, headers=headers)
+    except:
+        raise
+    try:
+        soup = BeautifulSoup(r.content.decode("utf-8"), 'lxml')
+    except UnicodeDecodeError:
+        soup = BeautifulSoup(r.text, 'lxml')
+    return soup
+
+
+class Downloader(object):
+    """
+    一个异步下载器，可以对代理源异步抓取，但是容易被BAN。
+    """
+    def __init__(self, urls):
+        self.urls = urls
+        self._htmls = []
+
+    async def download_single_page(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                self._htmls.append(await resp.text())
+
+    def download(self):
+        loop = asyncio.get_event_loop()
+        tasks = [self.download_single_page(url) for url in self.urls]
+        loop.run_until_complete(asyncio.wait(tasks))
+
+    @property
+    def htmls(self):
+        self.download()
+        return self._htmls
